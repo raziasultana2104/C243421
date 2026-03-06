13891e77843b63e9a3a3ec4dde1cb4630194a14b
unknown <raziasultana.rs2104@gmail.com>
2026-03-06 20:56:19 +0600

    -_-

--- lab3.cpp
+++ lab3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-void bubbleSort(int arr[], int n);
+void insertionSort(int arr[], int n);
 void inputArray(int arr[], int n);
 void display(int arr[], int n);
 int main()
@@ -10,22 +10,22 @@ int main()
     cin >> n;
     int arr[1000];
     inputArray(arr, n);
-    bubbleSort(arr, n);
+    insertionSort(arr, n);
     display(arr,n);
     
 }
-void bubbleSort(int arr[], int n)
+void insertionSort(int arr[], int n)
 {
     int t= 0;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n-i-1;j++)
+        for(int j=i;j>0;j--)
         {
-            if(arr[j]>arr[j+1])
+            if(arr[j]<arr[j-1])
             {
                 t=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=t;
+                arr[j]=arr[j-1];
+                arr[j-1]=t;
             }
         }
     }
